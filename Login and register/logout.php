50d352a603c1ab2c84dc60ad34ca8e843b00d6c2
junscribbles <102041663+junscribbles@users.noreply.github.com>
2023-01-18 11:13:21 +0100

    delete nathans projekt

--- Login and register/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-// initialize session
-session_start();
-// unset all existing session
-session_unset();
-// destroy all session to logout user
-session_destroy();
-// redirect user to home page
-header("location: ./");
